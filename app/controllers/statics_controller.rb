33803eb34d65f9b01242c2b56c7a307ec4138323
sarah wang <sarahwang47@gmail.com>
2020-08-27 09:06:16 +0800

    session

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,6 +1,11 @@
 class StaticsController < ApplicationController
   def page
     @user = User.new
-    @products = Product.paginate(page: params[:page])
+    @categories = Categorie.all
+    if params[:categorie].present?
+      @products = Categorie.find_by(name: params[:categorie]).products.paginate(page: params[:page])
+    else
+      @products = Product.paginate(page: params[:page])
+    end
   end
 end
