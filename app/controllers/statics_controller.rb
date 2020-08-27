059afca5908ecc6cedb6850a11d48ffadc824549
sarah wang <sarahwang47@gmail.com>
2020-08-27 09:34:12 +0800

    added remote for search per categorie

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -2,10 +2,14 @@ class StaticsController < ApplicationController
   def page
     @user = User.new
     @categories = Categorie.all
-    if params[:categorie].present?
-      @products = Categorie.find_by(name: params[:categorie]).products.paginate(page: params[:page])
-    else
-      @products = Product.paginate(page: params[:page])
+      if params[:categorie].present?
+        @products = Categorie.find_by(name: params[:categorie]).products.paginate(page: params[:page])
+      else
+        @products = Product.paginate(page: params[:page])
+      end
+    respond_to do |format|
+      format.js
+      format.html
     end
   end
 end
